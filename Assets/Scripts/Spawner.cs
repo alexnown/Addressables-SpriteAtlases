9d8008f13ff093cf1d3d1b5e8439a9e5c32e9fbe
serovoy <alexander.serovoy@gmail.com>
2018-10-16 23:34:25 +0300

    init

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace alexnown.AddressablesAndAtlases
+{
+    public class Spawner : MonoBehaviour
+    {
+        public GameObject[] Prefabs;
+
+        private void Start()
+        {
+            for (int i = 0; i < Prefabs.Length; i++)
+            {
+                Instantiate(Prefabs[i], 3 * Random.insideUnitCircle, Quaternion.identity);
+                Loader.Log($"{Time.frameCount}: {Prefabs[i].name} prefab instantiated.");
+            }
+        }
+    }
+}
